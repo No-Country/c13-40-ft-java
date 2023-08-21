9692c67575e82854003cccaf8df034d4642f2711
alexmartin <alexeusebiomartin@gmail.com>
2023-08-21 18:50:12 -0300

    Navbar and first frontend commit

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,12 @@
 
+
 const Home = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-2xl font-bold">Hola c13-40-ft-java</h1>
-    </div>
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-2xl font-bold">Hola c13-40-ft-java</h1>
+      </div>
+    </>
   );
 };
 
