bccc534f9a0272fc0c06e6d6e3ce8955cff128b3
narahumano1989 <humano.naraesther@gmail.com>
2023-08-22 11:25:06 -0300

    se sube estructura para armar el modelo y repositorio

--- backend/api-nocountry/src/main/java/com/ecommerce/apinocountry/controllers/products.java
+++ backend/api-nocountry/src/main/java/com/ecommerce/apinocountry/controllers/products.java
@@ -1,13 +1,14 @@
 package com.ecommerce.apinocountry.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class products {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
         public String Home(){
 
         return "Servicio API No Country";
