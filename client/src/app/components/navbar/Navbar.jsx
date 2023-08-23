f3b3525901482e6cc9247a42ec29d0cccbe832a2
alexmartin <alexeusebiomartin@gmail.com>
2023-08-22 22:00:51 -0300

    Navbar according to figma

--- client/src/app/components/navbar/Navbar.jsx
+++ client/src/app/components/navbar/Navbar.jsx
@@ -1,25 +1,51 @@
 "use client";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { BiCategory } from "react-icons/bi";
+import { FaRegHeart } from "react-icons/fa";
+import Image from "next/image";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="w-full h-screen relative">
-      <div className="sticky h-20 w-full bg-blue-400 flex justify-between items-center p-2">
+      <div className="sticky h-20 w-full bg-black flex justify-between items-center p-4">
         <GiHamburgerMenu
           onClick={() => setOpen(!open)}
-          className="text-3xl cursor-pointer md:hidden"
+          className="text-2xl text-white cursor-pointer md:hidden"
         />
-        <input className="h-6 w-32 rounded-full" type="text" />
-        <div className="flex justify-center items-center"></div>
+        <Image
+          className="mx-auto"
+          src="/LogoNav.png"
+          width={100}
+          height={100}
+        />
+        <div className="flex justify-center items-center">
+          <AiOutlineSearch className="text-2xl text-white cursor-pointer" />
+          <AiOutlineShoppingCart className="text-2xl mx-1 text-white cursor-pointer" />
+          <HiOutlineUserCircle className="text-2xl text-white cursor-pointer" />
+        </div>
       </div>
       <div
         className={`absolute top-0 mt-20 ${
           open ? "translate-x-[-900px]" : "translate-x-0"
-        } h-40 w-full bg-green-700 transition-all duration-500 ease`}
-      ></div>
+        } md:hidden h-screen w-6/12 bg-black transition-all duration-700 ease`}
+      >
+        <div className="flex flex-col justify-center items-start p-4">
+          <div className="flex flex-row my-2 cursor-pointer">
+            <BiCategory className="text-2xl text-white" />
+            <span className="text-white ml-2">Categories</span>
+          </div>
+          <div className="flex flex-row my-2 cursor-pointer">
+            <FaRegHeart className="text-2xl text-white" />
+            <span className="text-white ml-2">Favorites</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
