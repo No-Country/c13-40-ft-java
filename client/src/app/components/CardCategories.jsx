bac7a7e1c3b05707a8ea015ac5e294d1dacb7731
Joaquin <joaqobianchi13@gmail.com>
2023-08-25 00:55:52 -0300

    added responsive design to  cards

--- client/src/app/components/CardCategories.jsx
+++ client/src/app/components/CardCategories.jsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 function CardCategories({ Categorie, imageUrl }) {
   return (
     <article
-      className="w-4/5 h-48 mt-8 rounded grid place-items-center drop-shadow-xl bg-cover bg-no-repeat bg-center"
+      className="w-4/5 h-48 lg:w-[480px] lg:h-[580px]  mt-8 rounded grid place-items-center drop-shadow-xl bg-cover bg-no-repeat bg-center
+       transition ease-in-out duration-1000 delay-75 hover:saturate-200"
       style={{ backgroundImage: `url("${imageUrl}")` }}
     >
       <Link href="" className="text-3xl text-gray-50 font-sans font-extrabold">
