2126ea3c3b888c1904afe7a7618ab7036f7aacd6
Joaquin-Bianchi <105227524+Joaquin-Bianchi@users.noreply.github.com>
2023-08-25 18:14:01 -0300

    Revert "Joaquin bianchi"

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,8 +1,9 @@
+
 const Home = () => {
   return (
-    <>
-      <div className="w-full h-screen flex justify-center items-center"></div>
-    </>
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Hola c13-40-ft-java</h1>
+    </div>
   );
 };
 
