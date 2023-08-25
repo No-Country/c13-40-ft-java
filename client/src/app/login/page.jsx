9d4c4cc9560bcf26d113e02f4e71cfae2e82e4b9
Faradar <tomi_compuamste@hotmail.com>
2023-08-25 20:01:21 -0300

    Deleted login2 and updated public folder location

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "../public/logo.svg";
+import Logo from "public/logo.svg";
 import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
