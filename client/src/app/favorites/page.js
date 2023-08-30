8edd302f7014409af6c723a75ab8b957fd920e72
Joaquin <joaqobianchi13@gmail.com>
2023-08-30 18:48:56 -0300

    added favorties page and favoritesItem

--- client/src/app/favorites/page.js
+++ client/src/app/favorites/page.js
@@ -1,38 +1,40 @@
+import CartItem from "@/components/Cards/CartItem";
+import FavoritesItem from "@/components/Cards/FavoritesItem";
 import Link from "next/link";
+import React from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import CartItem from "../../components/cards/CartItem";
 
-export default function Cart() {
+function Favorites() {
   return (
     <section className="flex flex-col w-full mb-6">
       <div className="flex flex-col justify-center xl:mx-7  items-center w-full mt-10 mb-10">
-        <p className="text-5xl font-ArchivoBlack  font-extrabold">YOUR CART</p>
+        <p className="text-5xl font-ArchivoBlack  font-extrabold">FAVORITES</p>
         <div className="flex items-center">
           <Link
             href="/"
             className="flex items-center cursor-pointer underline decoration-black hover:opacity-70  font-serif 
-            duration-200"
+        duration-200"
           >
             <AiOutlineArrowLeft size={"20px"} className="cursor-pointer  " />
             Continue Shoping
           </Link>
           {/* <Link
-            href="/"
-            className="underline decoration-black hover:opacity-70 font-thin font-serif"
-          >
-            Continue Shoping
-          </Link> */}
+        href="/"
+        className="underline decoration-black hover:opacity-70 font-thin font-serif"
+      >
+        Continue Shoping
+      </Link> */}
         </div>
       </div>
 
-      <table className="table-auto drop-shadow-xl mx-3 md:mx-32 lg:mx-52 xl:mx-20   rounded-md">
+      <table className="table-auto drop-shadow- mx-3 md:mx-32 lg:mx-52 xl:mx-20   rounded-md">
         <thead>
           <tr className="ml-6 border-b">
             <th className="text-left  text-sm opacity-60 font-semibold">
               PRODUCT
             </th>
             <th className="hidden sm:table-cell text-center text-sm opacity-60 font-semibold">
-              QUANTITY
+              ADD
             </th>
             <th className="text-center text-sm opacity-60 font-semibold ">
               PRICE
@@ -40,41 +42,13 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <CartItem />
-          <CartItem />
-          <CartItem />
+          <FavoritesItem />
+          <FavoritesItem />
+          <FavoritesItem />
         </tbody>
-        {/* refactor foot o crear una nueva tabla */}
-        <tfoot>
-          <tr className="">
-            <th className="text-left pt-8 text-xl font-ArchivoBlack font-medium xl:px-96">
-              Subtotal
-            </th>
-            <th className="text-center pt-8 text-xl font-medium ">$449.70</th>
-            <th className="text-center   text-xl  font-semibold  "></th>
-          </tr>
-          <tr className="">
-            <th className="text-left text-xl font-ArchivoBlack font-medium xl:px-96">
-              Delivery Fee
-            </th>
-            <th className="text-center text-xl font-ArchivoBlack font-medium ">
-              $50.00
-            </th>
-            <th className="text-center text-xl  font-semibold  "></th>
-          </tr>
-          <tr className="">
-            <th className="text-left text-xl font-ArchivoBlack font-semibold xl:px-96">
-              TOTAL
-            </th>
-            <th className="text-center text-xl font-ArchivoBlack font-semibold ">
-              $499.70
-            </th>
-            <th className="text-center text-xl  font-semibold  "></th>
-          </tr>
-        </tfoot>
       </table>
-      {/* boton de Checkout */}
-      <div className="flex justify-center items-center">
+      {/* boton de Accion */}
+      {/* <div className="flex justify-center items-center">
         <a
           href="#_"
           className="inline-flex items-center justify-center w-full px-6 xl:px-10 py-3 mb-2 xl:text-xl text-black bg-orange-100 border border-black hover:bg-orange-200 sm:w-auto sm:mb-0"
@@ -93,7 +67,9 @@ export default function Cart() {
             ></path>
           </svg>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 }
+
+export default Favorites;
