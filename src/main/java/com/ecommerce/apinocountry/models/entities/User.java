29557ec8107055bd7f7feccd2dabf560e02d0fce
Mgodoyd <godoymario110@gmail.com>
2023-09-05 16:20:00 -0600

    modification final

--- src/main/java/com/ecommerce/apinocountry/models/entities/User.java
+++ src/main/java/com/ecommerce/apinocountry/models/entities/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.PositiveOrZero;
 
-import java.util.Date;
 
 @Entity
 @Table(name = "users")
