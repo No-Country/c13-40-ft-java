ea17c8ffaf98620e25636c95a47bd855d12ed573
alexmartin <alexeusebiomartin@gmail.com>
2023-09-06 18:58:11 -0300

    Home view and active links

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -28,15 +28,31 @@ const Home = () => {
       ) : (
         <div
           data-aos="fade"
-          className="w-full h-full flex justify-start items-center flex-col mt-8"
+          className="w-full h-full flex justify-start items-center flex-col"
         >
-          <Image
-            className="h-40 lg:h-72 object-cover"
-            src="/Garden.png"
-            width={2000}
-            height={500}
-            alt="portada"
-          />
+          <div className="w-full lg:h-5/6 flex justify-center items-center bg-primary lg:rounded-b-3xl">
+            <div className="hidden md:block md:w-6/12 md:h-full flex flex-col justify-center items-center pl-20">
+              <h2 className="xl:text-6xl text-3xl -mt-12 mb-2">Exclusive</h2>
+              <h2 className="xl:text-6xl text-3xl mb-8">
+                Furniture Collection
+              </h2>
+              <p className="mb-8 text-xs md:text-base">
+                Explore different categories. Find the best deals.
+              </p>
+              <button className="bg-secondary text-black font-bold py-4 px-12 rounded-md">
+                Shop now
+              </button>
+            </div>
+            <div className="w-full md:w-6/12 h-auto md:mb-16 md:mt-8">
+              <Image
+                className="w-full md:w-8/12 md:rounded-md"
+                src="/home.png"
+                width={1000}
+                height={1000}
+                alt="portada"
+              />
+            </div>
+          </div>
           <Slider />
           <Image
             className="md:hidden mt-8 h-8"
