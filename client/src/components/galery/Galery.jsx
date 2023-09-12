aed4d96d8cc8e63d4eafbc7b0cfc4dcbcb7b9695
Joaquin <joaqobianchi13@gmail.com>
2023-09-12 13:15:52 -0300

    change color footer and  added links

--- client/src/components/galery/Galery.jsx
+++ client/src/components/galery/Galery.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Galery() {
   return (
-    <div className="mx-auto px-5 py-2 lg:px-32 lg:pt-24 mb-10">
+    <div className="mx-auto px-5 py-2 lg:px-32 lg:pt-24 mb-16">
       <div className="flex justify-center">
         <h4 className="text-xl  lg:text-3xl font-bold mb-44  sm:mb-10">
           Takes your inspiration to another level
