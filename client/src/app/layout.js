fa57a92b56c39ceec684c12155f1d67e02f6435a
Joaquin-Bianchi <105227524+Joaquin-Bianchi@users.noreply.github.com>
2023-09-16 23:12:59 -0300

    Joaquin bianchi (#50)
    * traducciones y precios correjidos
    
    * added Title and favicon

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -15,16 +15,25 @@ import { AuthContextProvider } from "@/context/AuthContext";
 
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
-// export const metadata = {
-//   title: "No-Country",
-//   description: "c13-40-ft-java e-commerce",
-// };
+export const metadata = {
+  title: "Comfy Store",
+  description:
+    "Comfy Store: Tu destino en línea para muebles de alta calidad. Explora una amplia gama de muebles para el hogar, desde sofás cómodos hasta elegantes mesas de comedor. Descubre opciones de diseño moderno y clásico que se adaptan a cualquier estilo de decoración. ¡Encuentra la comodidad que necesitas para tu hogar en Comfy Store!",
+};
 
 export default function RootLayout({ children }) {
   const queryClient = new QueryClient();
 
   return (
     <html lang="en">
+      <head>
+        <title>Comfy Store</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+      </head>
       <body className={roboto.className}>
         <QueryClientProvider client={queryClient}>
           <AuthContextProvider>
